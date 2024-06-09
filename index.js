4c3500f2779145d7f58792bcaf3eaecea6cbacff
Tawkir711 <mdtawkirk711@gmail.com>
2024-06-10 01:33:54 +0600

    Use the get and post method server side

--- index.js
+++ index.js
@@ -27,11 +27,22 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const spotCollection = client.db('spotDB').collection('spot');
+
+    app.get('/spot', async (req, res) => {
+      const cursor = spotCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/spot', async (req, res) => {
+      const newSpot = req.body;
+      console.log(newSpot);
+      const result = await spotCollection.insertOne(newSpot);
+      res.send(result);
+    })
 
 
-
-
-    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
