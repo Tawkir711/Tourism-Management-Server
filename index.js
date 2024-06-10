eb94fc7b0eb9d12bf578e7e3abd7cd8de16f44b0
Tawkir711 <mdtawkirk711@gmail.com>
2024-06-10 18:31:41 +0600

    Mylist get, post and delete method

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+  ],
+  credentials: true
+}));
 app.use(cors());
 app.use(express.json());
 
@@ -28,6 +34,7 @@ async function run() {
     await client.connect();
 
     const spotCollection = client.db('spotDB').collection('spot');
+    const myListCollection = client.db('spotDB').collection('List');
 
     app.get('/spot', async (req, res) => {
       const cursor = spotCollection.find();
@@ -49,6 +56,43 @@ async function run() {
       res.send(result);
     })
     
+    // app.get('/myLists', async (req, res) => {
+    //   console.log(req.query.email);
+    //   console.log('cok cok', req.user);
+    //   if (req.user && req.query && req.user.email && req.query.email && req.user.email !== req.query.email) {
+    //     return res.status(403).send({message: 'forbidden access'})
+    //   }
+    //   else {
+    //     // f
+    //   }
+    //   let query = {};
+    //   if (req.query?.email) {
+    //     query= {email: req.query.email}
+    //   }
+    //   console.log(req.user.email);
+    //   console.log(req.query.email);
+    //   const result = await myListCollection.find(query).toArray();
+    //   res.send(result);
+    // })
+
+    app.get('/myLists', async (req, res) => {
+      const cursor = myListCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/myLists', async (req, res) => {
+      const list = req.body;
+      const result = await myListCollection.insertOne(list);
+      res.send(result);
+    })
+
+    app.delete('/myLists/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await myListCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
     // Send a ping to confirm a successful connection
