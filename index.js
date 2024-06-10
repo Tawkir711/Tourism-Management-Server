abd18768403574658b0105ed8a660db182248732
Tawkir711 <mdtawkirk711@gmail.com>
2024-06-10 13:52:04 +0600

    use get method for spot detailes single pade

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -35,12 +35,20 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/spot/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await spotCollection.findOne(query)
+      res.send(result);
+    })
+
     app.post('/spot', async (req, res) => {
       const newSpot = req.body;
       console.log(newSpot);
       const result = await spotCollection.insertOne(newSpot);
       res.send(result);
     })
+    
 
 
     // Send a ping to confirm a successful connection
